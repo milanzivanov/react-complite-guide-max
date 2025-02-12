23eb89dfaf21d7d4ca3726997fbda12fdbc8019d
milan <milanzivanov@users.noreply.github.com>
2025-02-12 21:08:18 +0100

    Using ref for more than DOM element conections

--- 08-refPortals/src/App.jsx
+++ 08-refPortals/src/App.jsx
@@ -1,10 +1,16 @@
-import Player from './components/Player.jsx';
+import Player from "./components/Player.jsx";
+import TimerChallenge from "./components/TimerChallenge.jsx";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easer Challenge" targetTime={1} />
+        <TimerChallenge title="Medium Challenge" targetTime={5} />
+        <TimerChallenge title="Hard Challenge" targetTime={10} />
+        <TimerChallenge title="Insane Challenge" targetTime={10} />
+      </div>
     </>
   );
 }
