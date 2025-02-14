7f1955a7c813fa04f5e93b0ad46d35d1eb35cf67
milan <milanzivanov@users.noreply.github.com>
2025-02-14 12:51:48 +0100

    fixin some logic bug

--- 08-refPortals/src/components/Player.jsx
+++ 08-refPortals/src/components/Player.jsx
@@ -11,8 +11,10 @@ export default function Player() {
 
   return (
     <section id="player">
-      {/* <h2>Welcome {enteredPlayerName ? enteredPlayerName : "unknown entity"}</h2> */}
-      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
+      <h2>
+        Welcome {enteredPlayerName ? enteredPlayerName : "unknown entity"}
+      </h2>
+      {/* <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2> */}
       <p>
         <input type="text" ref={playerName} />
         <button onClick={handleClick}>Set Name</button>
